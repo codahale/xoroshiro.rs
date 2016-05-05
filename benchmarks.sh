47c4f5cfa206a95734619c5ea2279cb6e4111065
Coda Hale <coda.hale@gmail.com>
2016-05-04 18:36:38 -0700

    Pretty up benchmark script.
    This only works on MacOS w/ pngcrush installed.

--- benchmarks.sh
+++ benchmarks.sh
@@ -1,2 +1,5 @@
 #!/bin/sh
+rm -rf .criterion
 multirust run nightly cargo bench --features benchmarks -- --test --nocapture
+qlmanage -t -s 1000 -o /tmp .criterion/Rng/summary/new/violin_plot.svg
+pngcrush /tmp/violin_plot.svg.png results.png
